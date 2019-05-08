ad173dbc2dc2ba12626d2004589e521bcb6c851f
Bahiyah <bahiyahperumbadiyil6@gmail.com>
2019-05-08 09:29:20 +0530

    Inial Commit

---
+++ Validation two/Validation two/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Validation;
+
+namespace Validation_two
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Validation.Validation v = new Validation.Validation();
+            if (!v.required(txtName.Text))
+            {
+                label3.Text = "Field is required ";
+            }
+            
+            if(!v.PhoneValidation(TxtPhone.Text))
+            {
+                label4.Text = "This phone number is not valid";
+            }
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+                
+        }
+    }
+}
---
+++ Validation/Validation/Validation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Validation
+{
+   public class Validation
+    {
+        
+        public  bool required(string text1)
+        {
+            if ( text1==null||text1=="")
+            {
+                return false;
+                
+            }
+            
+            else
+            { 
+            
+                return true;
+            };
+        }
+        public bool PhoneValidation(string text2 )
+        {
+
+            if (text2.Length == 10)
+            {
+                return true;
+            }
+            else if(text2.Length>10)
+            {
+                return false;
+            }
+            else if(text2.Length<10)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
+    }
+}
